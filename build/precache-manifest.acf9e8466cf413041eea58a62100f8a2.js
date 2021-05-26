eed9b3c2575cdd81bc832b2584b73ca5383b5251
Navya-Andra <anchowdary13@gmail.com>
2021-05-26 12:04:04 +0530

    first commit

---
+++ build/precache-manifest.acf9e8466cf413041eea58a62100f8a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdab793c71db7074c6747c532ebd1598",
+    "url": "/index.html"
+  },
+  {
+    "revision": "15854ef9d19899ccc42b",
+    "url": "/static/css/main.3c2fef4c.chunk.css"
+  },
+  {
+    "revision": "c57fc4786e845e3ea61c",
+    "url": "/static/js/2.f96aadd2.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.f96aadd2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15854ef9d19899ccc42b",
+    "url": "/static/js/main.0c5c4e5b.chunk.js"
+  },
+  {
+    "revision": "fae5941e4e5d78cbc530",
+    "url": "/static/js/runtime-main.4529b8ec.js"
+  }
+]);
